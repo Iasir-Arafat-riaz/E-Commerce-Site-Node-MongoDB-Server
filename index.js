966039c674e6d72cb93568470a85c5ae7814d213
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2021-11-04 02:19:20 +0600

    pagination set complete

--- index.js
+++ index.js
@@ -1,50 +1,64 @@
-const { MongoClient } = require('mongodb');
-const express = require("express")
+const { MongoClient } = require("mongodb");
+const express = require("express");
 require("dotenv").config();
-const cors = require("cors")
-const app = express()
+const cors = require("cors");
+const app = express();
 
 //adding middleware
-app.use(cors())
-app.use(express.json())
-const port = process.env.PORT||5000;
-
+app.use(cors());
+app.use(express.json());
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gukqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
-    try {
-      await client.connect();
-      const database = client.db("Ema-jhonDB");
-      const allProducts = database.collection("products");
-      // query for movies that have a runtime less than 15 minutes
+  try {
+    await client.connect();
+    const database = client.db("Ema-jhonDB");
+    const allProducts = database.collection("products");
+    // query for movies that have a runtime less than 15 minutes
+    const query = {};
+    app.get("/shop", async (req, res) => {
+      console.log(req.query);
       const query = {};
-      app.get("/shop",async(req,res)=>{
-        const query = {}
-        const cursor = allProducts.find(query)  
-        const all = await cursor.toArray()
-        res.send(all)
-      })
-     
-     //Post operation
+      const cursor = allProducts.find(query);
+      const pageNumber = req.query.page;
+      const size = parseInt(req.query.size);
+      
+      
+      const count = await cursor.count()
+      let products;
+      if (pageNumber) {
+        products = await cursor
+          .skip(pageNumber * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+
+      res.send({ products, count });
+    });
+
+    //Post operation
     //  app.post("shop",async(req,res)=>{
     //    res.send("new one")
     //  })
-
-     
-    } finally {
+  } finally {
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("yes sending connected yaya");
+});
 
-app.get("/",(req,res)=>{
-    res.send("yes sending connected yaya")
-})
-
-app.listen(port,()=>{
-    console.log("lestening to this port",port)
-})
+app.listen(port, () => {
+  console.log("lestening to this port", port);
+});
